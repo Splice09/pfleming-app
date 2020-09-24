5bf627ce3027e5fad16196d08ef9a899c7b24960
Splice <jpf0005@gmail.com>
2020-09-24 00:38:59 -0400

    aligning homepage divs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './components/app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { LearningComponent } from './components/learning/learning.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BlogComponent,
+    LearningComponent
   ],
   imports: [
     BrowserModule,
